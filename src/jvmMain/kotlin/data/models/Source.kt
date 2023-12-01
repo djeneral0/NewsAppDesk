ccab02ece6743fcda180a92db7d35b3a11087235
adutwum-soft <patrick.adutwum@protonmail.ch>
2023-12-01 23:33:46 +0000

    news models setup

---
+++ src/jvmMain/kotlin/data/models/Source.kt
@@ -0,0 +1,13 @@
+package data.models
+
+
+import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Source(
+    @SerializedName("id")
+    val id: String?,
+    @SerializedName("name")
+    val name: String
+)
